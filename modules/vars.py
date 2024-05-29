f290edc6bc2ab04c6c50643ae6af54509435ecc3
Mohammadnaved Naved <36394261+navedmohammad@users.noreply.github.com>
2024-05-29 14:18:33 +0530

    Add files via upload

---
+++ modules/vars.py
@@ -0,0 +1,11 @@
+#𝐌𝐚𝐝𝐞 𝐁𝐲  : [𝙎𝙢𝙚𝙭𝙮 ᥫ᭡](https://t.me/SmexyOP)
+
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+
+api_id = "22475741"
+
+api_hash = "1a217be71a0225e0a678af286c211f8a"
+
+bot_token = "7381636200:AAGSpXuOVESR0gRgsq7_DcmdUHDkGitCpAc"
+
+# Bot Username = "https://t.me/SmexyTxtLeechingBot"
